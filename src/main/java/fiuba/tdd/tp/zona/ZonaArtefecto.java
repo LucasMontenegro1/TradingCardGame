78599cbeb7fb631bd7fa07213d78c5b8fa96ef53
brusati <ignacio.brusati@gmail.com>
2023-06-02 08:51:01 -0300

    ADD: zona de artefactos

---
+++ src/main/java/fiuba/tdd/tp/zona/ZonaArtefecto.java
@@ -0,0 +1,30 @@
+package fiuba.tdd.tp.zona;
+
+public class ZonaArtefecto implements Zona {
+
+    @Override
+    public Zona cambiarZona() {
+        return new ZonaArtefecto();
+    }
+
+    @Override
+    public Zona moverACombate() {
+        return new ZonaArtefecto();
+    }
+
+    @Override
+    public Zona moverAReserva() {
+        return new ZonaArtefecto();
+    }
+
+    @Override
+    public Zona invocar() {
+        return new ZonaArtefecto();
+    }
+
+    @Override
+    public Zona descartar() {
+        return new ZonaDescarte();
+    }
+
+}
