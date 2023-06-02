91c97d4108c4c343ed5d890ca174d03e1296f49d
julietamponti <jponti@fi.uba.ar>
2023-06-02 12:32:56 -0300

    Agregado de la funcionalidad del turno

--- src/main/java/fiuba/tdd/tp/turno/Turno.java
+++ src/main/java/fiuba/tdd/tp/turno/Turno.java
@@ -1,4 +1,28 @@
 package fiuba.tdd.tp.turno;
 
+import fiuba.tdd.tp.etapa.Etapa;
+import fiuba.tdd.tp.etapa.EtapaInicial;
+import fiuba.tdd.tp.mazo.Mazo;
+import fiuba.tdd.tp.modo.Modo;
+
 public class Turno {
+    
+    private Etapa etapa; 
+
+    public Etapa etapaActual(){
+        return this.etapa;
+    }
+    
+    public Turno (Modo modo, Mazo mazo, Integer puntos){
+        this.etapa = new EtapaInicial(modo,mazo,puntos);    
+    }
+    
+    public void iniciarTurno(){
+        this.etapa.iniciar();
+    }
+
+    public void pasarDeEtapa(){
+        this.etapa = this.etapa.finalizar();
+    }
+
 }
