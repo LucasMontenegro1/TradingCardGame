a6d7db1be48d754369783596c39633487e873b78
julietamponti <jponti@fi.uba.ar>
2023-06-10 18:42:24 -0300

    Agrego funcionalidad del método TomarCarta

--- src/main/java/fiuba/tdd/tp/carta/Metodos/TomarCarta.java
+++ src/main/java/fiuba/tdd/tp/carta/Metodos/TomarCarta.java
@@ -1,11 +1,15 @@
 package fiuba.tdd.tp.carta.Metodos;
 
+import java.util.ArrayList;
 import java.util.Deque;
+import java.util.Random;
 
 import fiuba.tdd.tp.carta.Carta;
 import fiuba.tdd.tp.carta.Energia;
 import fiuba.tdd.tp.carta.Tipo;
 import fiuba.tdd.tp.etapa.Etapa;
+import fiuba.tdd.tp.etapa.EtapaInicial;
+import fiuba.tdd.tp.etapa.EtapaPrincipal;
 import fiuba.tdd.tp.tablero.Tablero;
 import fiuba.tdd.tp.zona.Zona;
 import fiuba.tdd.tp.zona.ZonaCombate;
@@ -26,14 +30,23 @@ public class TomarCarta implements MetodoCarta {
         if (this.tipo == Tipo.Criatura){
             return tipo.etapa.getClass() == etapa.getClass() && zona instanceof ZonaCombate;
         }
-        return tipo == Tipo.Artefacto && !(zona instanceof ZonaDescarte || zona instanceof ZonaMano);
+        return tipo == Tipo.Artefacto && (etapa instanceof EtapaPrincipal || etapa instanceof EtapaInicial) && !(zona instanceof ZonaMano);
     }
 
     @Override
     public void ejecutar(Tablero enJuego, Tablero contrincante, Deque<MetodoCarta> pilaMetodos, String jugadorObjetivo,
             Carta carta, Energia energia) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'ejecutar'");
+        
+                
+        for (int i = 0; i< this.cantidad; i++){
+            ArrayList<Carta> cartas = enJuego.cartasEnZona(null);
+            Random random = new Random();
+        
+            Carta cartaTomada = cartas.get(random.nextInt(cartas.size()));
+
+            cartaTomada.zona = new ZonaMano();
+        }
+
     }
 
 }
